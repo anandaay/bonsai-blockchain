fc77117139415124a09c67c54b973112943d00b9
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-08-03 19:26:39 +0700

    call a function inside contract
    call getText() function from deployed contract address

--- web_app.py
+++ web_app.py
@@ -8,6 +8,18 @@ from brownie.project.BrownieDirProject import *
 abi = TextStorage.abi
 bytecode = TextStorage.bytecode
 
+contract_address = '0x046dc538608FAa4D978FCFcfd3479e9FaE77aE42'
+
+#connect to blockchain server using web3 library
+from web3 import Web3
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+
+#contract address created after deploying contract
+targetContract = w3.eth.contract(address=contract_address, abi=abi)
+currentStoredText = targetContract.functions.getText().call() #getText function from contract
+
+print(currentStoredText)
+
 
 from flask import Flask
 
