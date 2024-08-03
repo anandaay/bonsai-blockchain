4a6521d5fe3f8d1bd8c4278564523384760dcaa6
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-08-03 17:21:59 +0700

    Create TextStorage.sol
    after creating contract, execute command : brownie compile
    
    there will be output .json in : build/contracts

---
+++ brownie-dir/contracts/TextStorage.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TextStorage {
+    string public stored_text;
+
+    constructor() {
+        setText('constructor text');
+    }
+
+    function setText(string memory _text) public {
+        stored_text = _text;
+    }
+
+    function getText() view public returns (string memory) {
+        return stored_text;
+    }
+}
