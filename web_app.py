99138317ad194a997dc005bc929c050d9810681e
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-08-03 19:41:32 +0700

    call blockchain function from route and python function

--- web_app.py
+++ web_app.py
@@ -8,24 +8,26 @@ from brownie.project.BrownieDirProject import *
 abi = TextStorage.abi
 bytecode = TextStorage.bytecode
 
+#contract address created after deploying contract
 contract_address = '0x046dc538608FAa4D978FCFcfd3479e9FaE77aE42'
 
 #connect to blockchain server using web3 library
 from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 
-#contract address created after deploying contract
-targetContract = w3.eth.contract(address=contract_address, abi=abi)
-currentStoredText = targetContract.functions.getText().call() #getText function from contract
+def getTextByContractAddress(ctx_addr):
+    targetContract = w3.eth.contract(address=ctx_addr, abi=abi)
+    currentStoredText = targetContract.functions.getText().call() #getText function from contract
 
-print(currentStoredText)
+    print(currentStoredText)
+    return currentStoredText
 
 
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    debugStr = "<p>Abi: " + str(abi) + "<br>Bytecode: " + str(bytecode) + "</p>"
-    return debugStr
+def index():
+    
+    stored_text = getTextByContractAddress(contract_address)   
+    return stored_text
