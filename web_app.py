ff8c7e6327b5d641e7e2a53596f6980110eb60fb
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-08-09 16:16:36 +0700

    setTextFunction

--- web_app.py
+++ web_app.py
@@ -22,6 +22,22 @@ def getTextByContractAddress(ctx_addr):
     print(currentStoredText)
     return currentStoredText
 
+def setTextInCtx(ctx_addr, sndr_addr, sndr_pk, value):
+    contract = w3.eth.contract(address=ctx_addr, abi=abi)
+    tx = contract.functions.setText(value).build_transaction({
+        'from' : sndr_addr,
+        'nonce' : w3.eth.get_transaction_count(sndr_addr),
+        'gasPrice': 200000
+    })
+
+    signed_tx = w3.eth.account.sign_transaction(tx, sndr_pk)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    print(tx_receipt)       
+    
+    return tx_receipt['transactionHash'].hex()
+
 
 from flask import Flask, request, render_template
 app = Flask(__name__)
@@ -77,7 +93,8 @@ def getTextByHash():
             
             tx_receipt = w3.eth.get_transaction_receipt(hash)
             print(tx_receipt)
-            ctx_addr = tx_receipt['contractAddress']   
+            #ctx_addr = tx_receipt['contractAddress']
+            ctx_addr = tx_receipt['to']     
             returned_text = getTextByContractAddress(ctx_addr) 
             
         else:
@@ -86,3 +103,38 @@ def getTextByHash():
         returned_text = 'Wrong Request Method'
           
     return returned_text
+
+@app.route("/set_text_in_ctx_addr", methods=['GET'])
+def setTextInContract():
+    returned_text = ''
+    ctx_addr = ''
+    sender_address = ''
+    sender_pk = ''
+    value = ''
+    hash = ''
+    # print(request.method)
+    if request.method == 'GET':
+        if request.args['ctx_addr']:
+            sender_address = request.args['sender_address']
+            ctx_addr = request.args['ctx_addr']
+            sender_pk = request.args['sender_pk']    
+            value = request.args['value']
+            
+        elif request.form['ctx_addr']:
+            sender_address = request.form['sender_address']
+            ctx_addr = request.form['ctx_addr']
+            sender_pk = request.form['sender_pk']    
+            value = request.form['value']  
+        
+        if ctx_addr is not '':                                 
+            returned_text = setTextInCtx(ctx_addr, sender_address, sender_pk, value)
+            
+        else:
+            returned_text = 'Wrong Parameter Input'
+                
+    else:
+        returned_text = 'error'
+
+    print(returned_text)
+    return returned_text
+    
