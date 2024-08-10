ebd6a844e21cd3d0e16b7ff1f6aa7c58c59aa8cf
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-08-10 23:52:43 +0700

    edit contract to get history in contract

--- brownie-dir/contracts/TextStorageOld.sol
+++ brownie-dir/contracts/TextStorageOld.sol
@@ -1,23 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract TextStorage {
+contract TextStorageOld {
     string public stored_text;
 
-    event TextChanged(string newText);
-
     constructor() {
-        setText('Constructor Text, default value when contract has been deployed.');
+        setText('constructor text');
     }
 
     function setText(string memory _text) public {
         stored_text = _text;
-        emit TextChanged(_text); 
     }
 
     function getText() view public returns (string memory) {
         return stored_text;
     }
-
-    
 }
